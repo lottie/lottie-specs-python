9bd41e30e59301be4e65f1cd61e26468940d59ce
Mattia Basaglia <glax@dragon.best>
2024-12-13 11:01:59 +0100

    Better handling of abstract types and property oneOf

--- src/lottie_specs/type_info.py
+++ src/lottie_specs/type_info.py
@@ -1,4 +1,5 @@
 import json
+import collections
 from .schema import Schema, SchemaPath
 
 
@@ -77,6 +78,17 @@ class Type:
     def __repr__(self):
         return "<%s %s %s>" % (self.__class__.__name__, self.schema.path, self.id)
 
+    def __eq__(self, o):
+        return isinstance(o, Type) and self.id == o.id
+
+
+class ListOfType:
+    def __init__(self, item_type: Type):
+        self.item_type = item_type
+
+    def __eq__(self, o):
+        return isinstance(o, ListOfType) and self.item_type == o.item_type
+
 
 class Property(Type):
     """
@@ -101,9 +113,26 @@ class Property(Type):
     def resolve(self):
         self.type = self.type_system.resolve_type(self.schema)
         if "items" in self.schema:
-            self.item_type = self.type_system.resolve_type(self.schema / "items")
-        else:
-            self.item_type = ""
+            self.type = ListOfType(self.type_system.resolve_type(self.schema / "items"))
+
+    def merge(self, duplicates):
+        type = self.type
+        for duplicate in duplicates:
+            duplicate.resolve()
+            other_type = duplicate.type
+            if type != other_type:
+                if not isinstance(type, list):
+                    type = [type, other_type]
+                elif other_type not in type:
+                    type.append(other_type)
+
+            if duplicate.const is not None:
+                self.const = None
+
+            if duplicate.description and duplicate.description not in self.description:
+                self.description += " or " + duplicate.description
+
+        self.type = type
 
 
 class Class(Type):
@@ -118,6 +147,8 @@ class Class(Type):
         self.properties = {}
         self.bases = []
         self.derived = []
+        self.concrete_descendants = []
+        self.duplicate_props = collections.defaultdict(list)
 
         if "allOf" in schema:
             for item in schema / "allOf":
@@ -125,6 +156,10 @@ class Class(Type):
                     self.base_refs.append(item.get("$ref"))
                 else:
                     self.get_properties(item)
+
+        if "oneOf" in schema:
+            for item in schema / "oneOf":
+                self.get_properties(item)
 
         self.get_properties(schema)
 
@@ -137,7 +172,11 @@ class Class(Type):
                     list(value.schema.keys())[0] == "not"
                 ):
                     continue
-                self.properties[name] = Property(self.type_system, value)
+                prop = Property(self.type_system, value)
+                if name in self.properties:
+                    self.duplicate_props[name].append(prop)
+                else:
+                    self.properties[name] = prop
 
     def resolve(self):
         for ref in self.base_refs:
@@ -145,8 +184,9 @@ class Class(Type):
             self.bases.append(base)
             base.derived.append(self)
 
-        for prop in self.properties.values():
+        for name, prop in self.properties.items():
             prop.resolve()
+            prop.merge(self.duplicate_props[name])
 
     def all_properties(self, props=None):
         if props is None:
@@ -166,13 +206,14 @@ class ConcreteClass(Type):
 
     def __init__(self, type_system: TypeSystem, schema: Schema):
         super().__init__(type_system, schema)
-        self.target_ref = self.ref.replace("/all-", "/")[:-1]
+        self.abstract_ref = self.ref.replace("/all-", "/")[:-1]
         self.concrete = []
 
     def resolve(self):
-        self.target = self.type_system.types[self.target_ref]
+        self.abstract = self.type_system.types[self.abstract_ref]
         for schema in self.schema / "oneOf":
             self.concrete.append(self.type_system.from_path(schema.get("$ref")))
+        self.abstract.concrete_descendants = self.concrete
 
 
 class EnumValue(Type):
