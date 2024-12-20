59030f99edbb100ac60cf5f9b66087f263ee8943
Mattia Basaglia <glax@dragon.best>
2024-12-20 10:47:11 +0100

    Code generation helper module

--- src/lottie_specs/__init__.py
+++ src/lottie_specs/__init__.py
@@ -1,4 +1,4 @@
-from . import schema, type_info, resources
+from . import schema, type_info, resources, codegen
 from .schema import Schema, SchemaPath
 from .type_info import TypeSystem
 from .resources import load_specs, schema_path
@@ -6,5 +6,6 @@ from .resources import load_specs, schema_path
 __all__ = [
     "schema", "Schema", "SchemaPath",
     "type_info", "TypeSystem",
-    "resources", "load_specs", "schema_path"
+    "resources", "load_specs", "schema_path",
+    "codegen",
 ]
